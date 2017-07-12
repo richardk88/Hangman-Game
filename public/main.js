8bf284e558a8c9014faae86cdc515d3592c0cb5a
richardk88 <richardk88@hotmail.com>
2017-07-12 15:11:49 -0400

    created buttons for each letter in the alphabet in main.js

---
+++ public/main.js
@@ -0,0 +1,28 @@
+//KEYBOARD
+//create an array for the alphabet
+var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+//loop through the alphabet and create buttons for each letter
+for (var i = 0; i < alphabet.length; i++) {
+	$('#alphabetButtons').append('<div class="btn btn-primary blue z-depth-3">' + alphabet[i] + '</div>');
+}
+
+
+//letter boxes
+
+
+//score tracker
+
+
+//remaining lives
+
+
+//correct
+
+
+//wrong
+
+
+//winner
+
+
+//loser
